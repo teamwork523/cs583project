74cce602dedfe0533ff7a17eba77d90ab4964280
tjandrew <tjandrew@tjandrew.eecs.umich.edu>
2012-12-16 16:42:25 -0500

    added correct CUT project

---
+++ CUT/lib/CUT.cpp
@@ -0,0 +1,266 @@
+
+#include <iostream>
+#include <fstream>
+#include <map>
+
+#include "llvm/Pass.h"
+#include "llvm/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Instructions.h"
+
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Analysis/ProfileInfo.h"
+#include "llvm/Analysis/Passes.h"
+#include "llvm/Analysis/LoopPass.h"
+#include "llvm/Support/CFG.h"
+#include "llvm/Support/Compiler.h"
+
+using namespace std;
+using namespace llvm;
+
+static std::map<Instruction*, Instruction*> *clone_map;
+static std::ofstream stat_file;
+
+static int function = 0;
+
+namespace
+{
+    struct CUT : public FunctionPass
+    {
+    
+      static char ID;
+      ProfileInfo* PI;
+
+      void Copy(Instruction *i);
+      void Cut(Function &F, list<BasicBlock*> *cut_list, map<BasicBlock*, BasicBlock*> *jump_list);
+      void Recurse(BasicBlock *jump, BasicBlock *block, map<BasicBlock*, BasicBlock*> *jump_list, list<BasicBlock*> *cut_list);
+
+      CUT() : FunctionPass(ID) {}
+      void getAnalysisUsage(AnalysisUsage &AU) const
+      {
+	      AU.addRequired<ProfileInfo>();
+      }
+
+      virtual bool runOnFunction(Function &F)
+      {
+          if(function == 0)
+          {
+              stat_file.open("stats.txt");
+          }
+          else
+          {
+              stat_file.open("stats.txt", std::fstream::app);
+          }
+
+          function++;
+
+          list<Instruction*>* copy_instructions = new list<Instruction*>();
+          clone_map = new std::map<Instruction*, Instruction*>();
+
+          for(Function::iterator b = F.begin(), be = F.end(); b != be; ++b)
+          {
+              for(BasicBlock::iterator I=b->begin(), ie = b->end(); I!=ie; ++I)
+              {
+                  unsigned opcode = I->getOpcode();
+
+                  //branch
+		          if(opcode >= 1 && opcode <= 7)
+                  {
+			          //do not copy branch instructions (they would be skipped anyway)
+                      //copy_instructions->push_back(&*I);
+		          }
+                  //memory
+		          else if(opcode >= 26 && opcode <= 32)
+		          {
+			          //memory instructions are protected by ECC
+                      if(opcode == 27)// || opcode == 28)
+                      {
+                          copy_instructions->push_back(&*I);
+                      }
+		          }
+                  //floating point ALU
+		          else if(opcode == 9 || opcode == 11 || opcode == 13 || 
+                    opcode == 16 || opcode == 19 || opcode == 36 || 
+                    opcode == 37 || opcode == 40 || opcode == 41 || 
+                    opcode == 46)
+		          {
+			          //floating point arithmetic must be checked
+                      copy_instructions->push_back(&*I);
+		          }
+                  //integer ALU
+		          else if(opcode == 8 || opcode == 10 || opcode == 12 || 
+                    opcode == 14 || opcode == 15 || opcode == 17 || 
+                    opcode == 17 || (opcode >= 20 && opcode <= 25) || 
+                    (opcode >= 33 && opcode <= 35) || opcode == 38 || 
+                    opcode == 39 || (opcode >= 42 && opcode <= 45))
+		          {
+			          //integer arithmetic must be checked
+                      copy_instructions->push_back(&*I);
+		          }
+                  //other
+		          else
+		          {
+                      //other instructions can cause errors and will
+                      //not be copied for the time being
+
+			          //other instructions will also be checked
+                      //copy_instructions->push_back(&*I);
+		          }	
+              }
+          }
+
+          for (list<Instruction*>::iterator I = copy_instructions->begin(), ie = copy_instructions->end(); I != ie; I++)
+          {
+              Copy(*I);
+          }
+
+          Instruction *first = *copy_instructions->begin();
+          BasicBlock *begin = first->getParent();
+          BasicBlock *split = SplitBlock(begin, begin->getFirstNonPHI(), this);
+          BranchInst::Create(split, begin->getTerminator());
+          begin->getTerminator()->eraseFromParent();
+
+          //return true;
+
+          list<BasicBlock*> *cut_list = new list<BasicBlock*>();
+          map<BasicBlock*, BasicBlock*> *jump_list = new map<BasicBlock*, BasicBlock*>();
+
+          int index = 0;
+          for(Function::iterator b = F.begin(), be = F.end(); b != be; ++b)
+          {
+              index++;
+              if(index == 3) cut_list->push_back(&*b);
+          }
+
+          Cut(F, cut_list, jump_list);
+
+          for (list<Instruction*>::iterator I = copy_instructions->begin(), ie = copy_instructions->end(); I != ie; I++)
+          {
+              Instruction *origi = *I;
+              Instruction *clone = (*clone_map)[*I];
+              begin = (*jump_list)[clone->getParent()];
+
+              //if(origi->getOpcode() > 19 || origi->getOpcode() < 8) continue;
+
+              BasicBlock *homeBB = clone->getParent();
+              BasicBlock *lastBB = SplitBlock(homeBB, homeBB->getTerminator(), this); 
+
+              stat_file << "A:" << clone->getName().str() << ":B:" << origi->getName().str() << ":C\n";
+
+              ICmpInst* compare = new ICmpInst(homeBB->getTerminator(), CmpInst::ICMP_EQ, clone, origi, "compare");
+              BranchInst::Create(lastBB, begin, compare, homeBB->getTerminator());
+              homeBB->getTerminator()->eraseFromParent();
+          }
+
+          stat_file.close();
+
+          return true;
+      }
+
+    };
+}
+
+char CUT::ID = 0;
+static RegisterPass<CUT> X("idemcut", "Idempotent Processign World Pass", false, false);
+
+void CUT::Cut(Function &F, list<BasicBlock*> *cut_list, map<BasicBlock*, BasicBlock*> *jump_list)
+{
+    int first = 0;
+    for(Function::iterator b = F.begin(), be = F.end(); b != be; ++b)
+    {
+        if(first == 0)
+        {
+            first = 1;
+            continue;
+        }
+        BasicBlock *block = &*b;
+        BasicBlock *jump = block;
+        //ignore blocks that have already been added
+        if(jump_list->find(block) != jump_list->end()) { stat_file << "ignore\n"; continue; }
+
+        stat_file << "start\n";
+
+        //recursively add children
+        Recurse(jump, block, jump_list, cut_list);
+    }
+
+    for(Function::iterator b = F.begin(), be = F.end(); b != be; ++b)
+    {
+        BasicBlock *block = &*b;
+        stat_file << "JUMP: " << (*jump_list)[block] << "\n";
+    }
+}
+
+void CUT::Recurse(BasicBlock *jump, BasicBlock *block, map<BasicBlock*, BasicBlock*> *jump_list, list<BasicBlock*> *cut_list)
+{
+	(*jump_list)[block] = jump;
+    TerminatorInst *term = block->getTerminator();
+
+    for (unsigned i = 0; i < term->getNumSuccessors(); i++)
+    {
+        BasicBlock *child = term->getSuccessor(i);
+
+    //for (BasicBlock::iterator b = block->begin(), be = block->end(); b != be; b++)
+    //{
+    //    BasicBlock *child = &*b;
+        int add_child = 1;
+        for (list<BasicBlock*>::iterator b = cut_list->begin(), be = cut_list->end(); b != be; b++)
+        {
+            BasicBlock *cut = *b;
+            if(child == cut) 
+            {
+                add_child = 0;
+            }
+        }
+        if(add_child == 1)
+        {
+            Recurse(jump, child, jump_list, cut_list);
+        }
+    }
+}
+
+void CUT::Copy(Instruction *i)
+{
+    //clone a new instruction
+    Instruction *clone = i->clone();
+    std::string name = i->getName().str() + ".clone";
+
+    if(!i->getType()->isVoidTy())
+    {
+        clone->setName(name);
+    }
+
+    clone->insertAfter(i); //DO NOT ADD HERE WHILE ITERATING!!!
+
+    stat_file << "clone\n";
+
+    //store the mapping between the original
+    //instruction and the clone
+    (*clone_map)[i] = clone;
+
+    //remove references to original registers
+    //from the cloned instruction operands
+    for (unsigned op = 0; op < clone->getNumOperands(); op++)
+    {
+        //examine each operand
+        Value *v = clone->getOperand(op);
+
+        //ignore non-instruction operands (only want registers)
+        if(!isa<Instruction>(v)) continue;
+
+        //cast the value as an instruction
+        Instruction *fix = dyn_cast<Instruction>(v);
+
+        //test if this is a mapped register
+        if(clone_map->find(fix) != clone_map->end())
+        {
+            stat_file << "##1:" << fix->getName().str() << " " << clone->getName().str() << ":2##\n";
+
+            //change the operand
+            clone->setOperand(op, (*clone_map)[fix]);
+        }
+    }
+}
+
+
+
---
+++ CUT/lib/Debug+Asserts/HWbu.d
@@ -0,0 +1,209 @@
+/home/tjandrew/Install/llvm/projects/HW1/lib/Debug+Asserts/HWbu.o \
+  /home/tjandrew/Install/llvm/projects/HW1/lib/Debug+Asserts/HWbu.d: \
+  HWbu.cpp /home/tjandrew/Install/llvm/include/llvm/Pass.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/Compiler.h \
+  /home/tjandrew/Install/llvm/include/llvm/PassSupport.h \
+  /home/tjandrew/Install/llvm/include/llvm/PassRegistry.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/StringRef.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/type_traits.h \
+  /home/tjandrew/Install/build/include/llvm/Support/DataTypes.h \
+  /home/tjandrew/Install/llvm/include/llvm/InitializePasses.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/Atomic.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/Valgrind.h \
+  /home/tjandrew/Install/build/include/llvm/Config/llvm-config.h \
+  /home/tjandrew/Install/llvm/include/llvm/PassAnalysisSupport.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/SmallVector.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/AlignOf.h \
+  /home/tjandrew/Install/llvm/include/llvm/Function.h \
+  /home/tjandrew/Install/llvm/include/llvm/GlobalValue.h \
+  /home/tjandrew/Install/llvm/include/llvm/Constant.h \
+  /home/tjandrew/Install/llvm/include/llvm/User.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/ErrorHandling.h \
+  /home/tjandrew/Install/llvm/include/llvm/Value.h \
+  /home/tjandrew/Install/llvm/include/llvm/Use.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/PointerIntPair.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/PointerLikeTypeTraits.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/Casting.h \
+  /home/tjandrew/Install/llvm/include/llvm/CallingConv.h \
+  /home/tjandrew/Install/llvm/include/llvm/BasicBlock.h \
+  /home/tjandrew/Install/llvm/include/llvm/Instruction.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/ilist_node.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/DebugLoc.h \
+  /home/tjandrew/Install/llvm/include/llvm/Instruction.def \
+  /home/tjandrew/Install/llvm/include/llvm/SymbolTableListTraits.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/ilist.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/Twine.h \
+  /home/tjandrew/Install/llvm/include/llvm/Argument.h \
+  /home/tjandrew/Install/llvm/include/llvm/Attributes.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/MathExtras.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/SwapByteOrder.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/ArrayRef.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/raw_ostream.h \
+  /home/tjandrew/Install/llvm/include/llvm/Instructions.h \
+  /home/tjandrew/Install/llvm/include/llvm/InstrTypes.h \
+  /home/tjandrew/Install/llvm/include/llvm/OperandTraits.h \
+  /home/tjandrew/Install/llvm/include/llvm/DerivedTypes.h \
+  /home/tjandrew/Install/llvm/include/llvm/Type.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/IntegersSubset.h \
+  /home/tjandrew/Install/llvm/include/llvm/Constants.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/APInt.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/APFloat.h \
+  /home/tjandrew/Install/llvm/include/llvm/LLVMContext.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/IntegersSubsetMapping.h \
+  /home/tjandrew/Install/llvm/include/llvm/Analysis/ProfileInfo.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/Debug.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/Format.h \
+  /home/tjandrew/Install/llvm/include/llvm/Analysis/Passes.h \
+  /home/tjandrew/Install/llvm/include/llvm/Analysis/LoopPass.h \
+  /home/tjandrew/Install/llvm/include/llvm/Analysis/LoopInfo.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/DenseMap.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/DenseMapInfo.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/DenseSet.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/DepthFirstIterator.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/GraphTraits.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/SmallPtrSet.h \
+  /home/tjandrew/Install/llvm/include/llvm/ADT/STLExtras.h \
+  /home/tjandrew/Install/llvm/include/llvm/Analysis/Dominators.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/CFG.h \
+  /home/tjandrew/Install/llvm/include/llvm/PassManagers.h \
+  /home/tjandrew/Install/llvm/include/llvm/Support/PrettyStackTrace.h \
+  /home/tjandrew/Install/llvm/projects/HW1/include/LAMP/LAMPLoadProfile.h \
+  /home/tjandrew/Install/llvm/projects/HW1/include/LAMP/LAMPProfiling.h
+
+/home/tjandrew/Install/llvm/include/llvm/Pass.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/Compiler.h:
+
+/home/tjandrew/Install/llvm/include/llvm/PassSupport.h:
+
+/home/tjandrew/Install/llvm/include/llvm/PassRegistry.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/StringRef.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/type_traits.h:
+
+/home/tjandrew/Install/build/include/llvm/Support/DataTypes.h:
+
+/home/tjandrew/Install/llvm/include/llvm/InitializePasses.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/Atomic.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/Valgrind.h:
+
+/home/tjandrew/Install/build/include/llvm/Config/llvm-config.h:
+
+/home/tjandrew/Install/llvm/include/llvm/PassAnalysisSupport.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/SmallVector.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/AlignOf.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Function.h:
+
+/home/tjandrew/Install/llvm/include/llvm/GlobalValue.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Constant.h:
+
+/home/tjandrew/Install/llvm/include/llvm/User.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/ErrorHandling.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Value.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Use.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/PointerIntPair.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/PointerLikeTypeTraits.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/Casting.h:
+
+/home/tjandrew/Install/llvm/include/llvm/CallingConv.h:
+
+/home/tjandrew/Install/llvm/include/llvm/BasicBlock.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Instruction.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/ilist_node.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/DebugLoc.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Instruction.def:
+
+/home/tjandrew/Install/llvm/include/llvm/SymbolTableListTraits.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/ilist.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/Twine.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Argument.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Attributes.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/MathExtras.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/SwapByteOrder.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/ArrayRef.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/raw_ostream.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Instructions.h:
+
+/home/tjandrew/Install/llvm/include/llvm/InstrTypes.h:
+
+/home/tjandrew/Install/llvm/include/llvm/OperandTraits.h:
+
+/home/tjandrew/Install/llvm/include/llvm/DerivedTypes.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Type.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/IntegersSubset.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Constants.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/APInt.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/APFloat.h:
+
+/home/tjandrew/Install/llvm/include/llvm/LLVMContext.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/IntegersSubsetMapping.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Analysis/ProfileInfo.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/Debug.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/Format.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Analysis/Passes.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Analysis/LoopPass.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Analysis/LoopInfo.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/DenseMap.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/DenseMapInfo.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/DenseSet.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/DepthFirstIterator.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/GraphTraits.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/SmallPtrSet.h:
+
+/home/tjandrew/Install/llvm/include/llvm/ADT/STLExtras.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Analysis/Dominators.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/CFG.h:
+
+/home/tjandrew/Install/llvm/include/llvm/PassManagers.h:
+
+/home/tjandrew/Install/llvm/include/llvm/Support/PrettyStackTrace.h:
+
+/home/tjandrew/Install/llvm/projects/HW1/include/LAMP/LAMPLoadProfile.h:
+
+/home/tjandrew/Install/llvm/projects/HW1/include/LAMP/LAMPProfiling.h:
